8e42dfac1cb5b42d078c182194923508a5d774cd
Darren <einn.dh@gmail.com>
2024-11-01 18:50:44 +0100

    Up1

--- Mobs.py
+++ Mobs.py
@@ -11,19 +11,6 @@ class Mob:
     def draw(self, surf):
         pygame.draw.rect(surf, self.color, self.rect)
 
-    def move(self):
-        if self.path_index < len(self.path)-1:
-            target = self.path[self.path_index+1] #On cible la prochain
-            dx,dy=target[0]-self.rect.x,target[1]-self.rect.y
-            dist=(dx**2+dy**2)**0.5
-            if(dist<=5):
-                self.path_index+=1
-            else:
-                self.rect.x+=5*dx/dist
-                self.rect.y+=5*dy/dist
-        else:
-            self.path_index=0
-
 
 
 
